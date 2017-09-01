8326c47f233fbc8e21f142e8b849f4fcebb15d6f
Riley MacDonald <riley.macdonald@redspace.com>
2017-09-01 16:42:31 -0300

    cleanup.

--- usb_hub_listener_udev.py
+++ usb_hub_listener_udev.py
@@ -4,22 +4,15 @@ import glib
 import os
 
 from pyudev import Context, Monitor
+from pyudev.glib import MonitorObserver
 
 usb_hub_hdmi = 'usb3/3-1/3-1.1/3-1.1:1.0'
 
-try:
-    from pyudev.glib import MonitorObserver
 
-    def device_event(observer, device):
-        update_monitor_settings(device)
-except:
-    from pyudev.glib import GUDevMonitorObserver as MonitorObserver
-
-    def device_event(observer, action, device):
-        update_monitor_settings(device)
+def device_event(observer, device):
+    update_monitor_settings(device)
 
 
-# print 'event {0} on device {1}'.format(action, device)
 def update_monitor_settings(device):
     if device.action.decode("string-escape") == "add":
         if usb_hub_hdmi in device.device_path.decode("string-escape"):
@@ -29,6 +22,7 @@ def update_monitor_settings(device):
         if usb_hub_hdmi in device.device_path.decode("string-escape"):
             os.system('xrandr --output DP-1 --off --output eDP-1 --mode 1920x1080 --rate 59.93 --same-as DP-1')
             print 'set monitor off'
+
 
 context = Context()
 monitor = Monitor.from_netlink(context)
